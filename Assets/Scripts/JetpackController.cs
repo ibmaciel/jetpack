d2c3a1fe3fcfa1eee22076e3e030f4326ff10e63
ibmaciel <ibmaciel@users.noreply.github.com>
2019-05-08 17:51:04 -0300

    Add some stuff

--- Assets/Scripts/JetpackController.cs
+++ Assets/Scripts/JetpackController.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class JetpackController : MonoBehaviour
 {
     public float jetpackForce = 75.0f;
+    public float forwardMovementSpeed = 3.0f;
 
+    public Transform groundCheckTransform;
+    private bool isGrounded;
+    public LayerMask groundCheckLayerMask;
+
+    public ParticleSystem jetpack;
+
+    private Animator jetpackAnimator;
     private Rigidbody2D playerRigidbody;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        
+        jetpackAnimator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -24,6 +28,34 @@ public class JetpackController : MonoBehaviour
         if (jetpackActive)
         {
             playerRigidbody.AddForce(new Vector2(0, jetpackForce));
+        }
+
+        Vector2 newVelocity = playerRigidbody.velocity;
+        newVelocity.x = forwardMovementSpeed;
+        playerRigidbody.velocity = newVelocity;
+
+        UpdateGroundedStatus();
+
+        AdjustJetpack(jetpackActive);
+    }
+
+    void UpdateGroundedStatus()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        jetpackAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        var jetpackEmission = jetpack.emission;
+        jetpackEmission.enabled = !isGrounded;
+        if (jetpackActive)
+        {
+            jetpackEmission.rateOverTime = 300.0f;
+        }
+        else
+        {
+            jetpackEmission.rateOverTime = 75.0f;
         }
     }
 }
