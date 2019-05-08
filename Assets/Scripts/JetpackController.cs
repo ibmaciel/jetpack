eb68138ded592b227fb169d2ecd8b30d5d452551
ibmaciel <ibmaciel@users.noreply.github.com>
2019-05-08 14:36:58 -0300

    Init

---
+++ Assets/Scripts/JetpackController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetpackController : MonoBehaviour
+{
+    public float jetpackForce = 75.0f;
+
+    private Rigidbody2D playerRigidbody;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+        if (jetpackActive)
+        {
+            playerRigidbody.AddForce(new Vector2(0, jetpackForce));
+        }
+    }
+}
